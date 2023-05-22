1895580bb9c7ab4fcebdf438606b893a46de3a52
Ariel Fuchs <ari.fuchs.5@gmail.com>
2023-05-22 10:29:29 -0400

    encoding begining

--- v0/encode.c
+++ v0/encode.c
@@ -2,15 +2,26 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 
-void encode(char *filename){
-    int fd = open(filename, O_RDONLY);
-    if(fd < 1){
-        printf("%s\n",strerror(errno));  
+void encode(char *filename, char *keyfile){
+    int filefd = open(filename, O_RDWR);
+    if(filefd < 1){
+        printf("text file%s\n",strerror(errno));  
         exit(1);  
     }
+    // key has to be 16 charectars long
+    int keyfd = open(keyfile, O_RDONLY);
+     if(keyfd < 1){
+        printf("key file %s\n",strerror(errno));  
+        exit(1);  
+    }
+    char key[128];
+    int arg = read(keyfd, key, 16 * sizeof(int));
+    printf("the bytes read %d")
+    
 }
 
 int main(){
-    encode("fdile");
+    encode("file", "key");
 }
